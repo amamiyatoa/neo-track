155aa6fc1f4f3a06683fad959020eb058158242a
amamiyatoa <hrt.mr@icloud.com>
2026-03-26 08:14:02 +0900

    new Commit

---
+++ static/js/index.js
@@ -0,0 +1,55 @@
+window.onload = () => {
+    window.addEventListener('scroll', () => {
+
+        //バナーの変数宣言//
+        const el = document.getElementById('HeightScanner');//HeightScannerから値を取得する
+        const rect = el.getBoundingClientRect().top;
+
+        //バナーフェードイン・アウト//
+        if(rect <= -50) {
+            const BannerTitleElement = document.querySelector('.BannerTitle');
+            BannerTitleElement.classList.remove('BannerTitleFadeout');
+            BannerTitleElement.classList.add('BannerTitleFade');
+        } else {
+            const BannerTitleElement = document.querySelector('.BannerTitle');
+            BannerTitleElement.classList.remove('BannerTitleFade');
+            BannerTitleElement.classList.add('BannerTitleFadeout');
+        }
+
+        //ヘッダー非表示//
+        if(rect <= -10) {
+            const HeaderElement = document.getElementById('topHeader');
+            HeaderElement.classList.remove('HeaderElementFade');
+            HeaderElement.classList.add('HeaderElementFadeout');
+        } else {
+            const HeaderElement = document.getElementById('topHeader');
+            HeaderElement.classList.remove('HeaderElementFadeout');
+            HeaderElement.classList.add('HeaderElementFade');
+        }
+
+        //コラムの変数宣言//
+        const col = document.getElementById('ColumnHeightScanner');
+        const colRect = col.getBoundingClientRect().top;
+        const ColumnElements = document.querySelectorAll('.column');
+
+        //コラムフェードイン・アウト//
+        if(colRect <= 600) {
+            ColumnElements.forEach((fadeEl,index) => {
+                setTimeout(() => {
+                    fadeEl.classList.remove('OrderHide');
+                    fadeEl.classList.add('OrderShow');
+                }, index * 350);                
+            });
+        }
+
+        //Topにスムーススクロール//
+        const SmoothScrollButton = document.getElementById('TopSmoothScrollButton');
+        SmoothScrollButton.addEventListener('click', () => {
+            window.scrollTo({
+                top: 0,
+                    behavior: 'smooth',
+            });
+        });
+
+    })
+}
